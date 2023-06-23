70751c84027ef7c67eb554264b84cc35ac791681
Preethy Ann Thomas <77205001+preethyannthomas@users.noreply.github.com>
2023-06-23 20:05:11 +0530

    Create Arrays - DS.py

---
+++ Arrays/Arrays - DS.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+def reverseArray(a):
+    # Write your code here
+    b = []
+    for i in range(len(a)-1,-1,-1):
+        b.append(a[i])
+    return b
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr_count = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    res = reverseArray(arr)
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+    fptr.close()
